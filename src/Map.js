a9d9543e38b4c83a081e43b15c9e3696e8fe066c
ryanboris <ryan.boris@gmail.com>
2018-11-09 00:54:20 -0500

    Feat: Creates and adds the Google Map API for view of baltimore as a component

---
+++ src/Map.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+
+/**
+ * Set value to true on map load callback
+ */
+function onMapLoaded() {
+    console.log('map callback');
+    window.isMapLoaded = true;
+}
+
+const MapComponent = withScriptjs(
+    withGoogleMap(props => {
+        return (
+            <GoogleMap
+                defaultZoom={10}
+                defaultCenter={props.places.length > 0 ? props.places[0] : { lat: 39.33182, lng: -76.6452 }}
+                defaultOptions={{ mapTypeControl: false }}
+                onClick={props.hideInfoWindow}
+            >
+                {props.isMarkerShown &&
+                    props.places.map((place, index) => (
+                        <Marker
+                            key={index}
+                            position={place}
+                            animation={place.clicked ? window.google.maps.Animation.BOUNCE : 0}
+                            onClick={() => {
+                                props.onMarkerClick(index);
+                            }}
+                        />
+                    ))}
+            </GoogleMap>
+        );
+    })
+);
+
+/**
+ * Map container with map component inside
+ */
+class Map extends Component {
+    componentDidMount() {
+        // Set initial value
+        window.isMapLoaded = false;
+
+        // Set map callback function
+        window.onMapLoaded = onMapLoaded;
+
+        // Set timeout for 10 seconds
+        setTimeout(() => {
+            if (!window.isMapLoaded) {
+                this.props.onError();
+            }
+        }, 10000);
+    }
+
+    render() {
+        return (
+            <div role="region" aria-label="map" className="map-container" style={{ marginLeft: '250px' }}>
+                <MapComponent
+                    isMarkerShown={this.props.places.length > 0}
+                    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC2eiZsx2oT_y03EA5ksk4zLCFq0mifCVM&v=3.exp&libraries=geometry,drawing,places&callback=onMapLoaded"
+                    loadingElement={<div style={{ height: `100%` }} />}
+                    containerElement={<div style={{ height: `100%` }} />}
+                    mapElement={<div style={{ height: `100%` }} />}
+                    places={this.props.places}
+                    hideInfoWindow={this.props.hideInfoWindow}
+                    onMarkerClick={this.props.onMarkerClick}
+                />
+            </div>
+        );
+    }
+}
+
+export default Map;
